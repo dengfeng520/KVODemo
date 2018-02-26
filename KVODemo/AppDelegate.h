ffe3dcbf4dc595fc33ddd384600ce67b862e30b0
dengfeng520 <deng_feng520@163.com>
2018-02-26 16:54:23 +0800

    Initial Commit

---
+++ KVODemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVODemo
+//
+//  Created by rp.wang on 2018/2/26.
+//  Copyright © 2018年 兰州北科维拓科技股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVODemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVODemo
+//
+//  Created by rp.wang on 2018/2/26.
+//  Copyright © 2018年 兰州北科维拓科技股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
